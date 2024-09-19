4d2a3a8452515c3d7e67e5bfdc3bf05271ceca97
DouaaAl <elmahraouidouaa123@gmail.com>
2024-09-19 20:45:00 +0100

    full app

---
+++ server.js
@@ -0,0 +1,46 @@
+import { Server } from "socket.io";
+import http from "http"; 
+
+const httpServer = http.createServer();
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: "*"
+    }
+});
+
+let onlineUsers = [];
+
+
+io.on("connection", (socket) => {
+
+    socket.on("newUser", (userId)=>{
+            onlineUsers = onlineUsers?.filter((item)=> item.userId != userId);
+            onlineUsers.push({userId: userId, socketId: socket.id});
+            io.emit("getAllLiveUsers", onlineUsers);
+    })
+
+    socket.on("message", (data)=>{
+        onlineUsers.forEach((user) =>{
+            if(user.userId == data.receiverId){
+                console.log("selected users:",user, data.receiverId);
+                io.to(user.socketId).emit("message", "realtime message")
+            }
+        })
+    })
+
+    socket.on("connectedUsers", ()=>{
+        io.emit("getAllLiveUsers", onlineUsers);
+    })
+
+    socket.on("disconnect", () => {
+        onlineUsers = onlineUsers.filter((item)=>{
+            return item.socketId != socket.id;
+        })
+        io.emit("getAllLiveUsers", onlineUsers);
+    });
+});
+
+httpServer.listen(3001, () => {
+    console.log("Server is working on port 3001");
+});
